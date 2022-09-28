f3e3f1a004d48e969bf240db159a7b0c54fcc920
ArunKhare <100008999+ArunKhare@users.noreply.github.com>
2022-09-28 17:23:42 +0530

    init_setup update

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START"
 echo [$(date)]: "Creating env with python 3.8 version"
-conda create --name env python=3.8 -y
+conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activating the environment"
-source activate env
+source activate ./env
 echo [$(date)]: "installing the dev requirements"
 pip install -r requirements_dev.txt
 echo [$(date)]: "END"
